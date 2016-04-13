3e7be56bce48674f3decf75580f2383cc8d7f6f1
Zac Konopa <konopaz@oregonstate.edu>
2016-04-12 21:55:02 -0700

    adding basic login form an session login
    super stupid login functionality - mostly  just verifying session handling is
    working. Right now there is no client or server side validation and you are
    just logged in as the username you provide.

--- application.py
+++ application.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, \
+  abort, render_template, flash
+import logging
+
+DEBUG = True
+LOGGING_LOCATION = 'application.log'
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
 application = Flask(__name__)
+application.config.from_object(__name__)
+#used for session cookie encryption
+application.secret_key = 'somesecretkeyhere'
+handler = logging.FileHandler(application.config['LOGGING_LOCATION'])
+handler.setLevel(application.config['LOGGING_LEVEL'])
+formatter = logging.Formatter(application.config['LOGGING_FORMAT'])
+handler.setFormatter(formatter)
+application.logger.addHandler(handler)
 
-@application.route("/")
-def hello():
-  return "Hello world!"
+@application.route('/')
+def home():
+  if 'username' in session:
+    return render_template('home.html')
+  return redirect(url_for('login'))
 
-if __name__ == "__main__":
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    application.logger.debug('handling login post')
+    session['username'] = request.form['username']
+    return redirect(url_for('home'))
+
+  return render_template('login.html')
+
+if __name__ == '__main__':
   application.run()
