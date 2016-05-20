ab6436def829be0b9603274887dfb8caa52023f0
Zac Konopa <konopaz@oregonstate.edu>
2016-05-19 21:34:00 -0700

    continuing email refactor

--- emprec/pdfcert.py
+++ emprec/pdfcert.py
@@ -11,6 +11,11 @@ class pdfcert:
   def __init__(self, imgfile):
     self.img = Image.open(imgfile)
 
+  def writeAward(self, award):
+    self.write(award['recipientName'], position=(250, 500), color=(10, 150, 77))
+    self.write(award['date'], position=(250, 600), color=(10, 150, 77))
+    self.write(award['type'], position=(250, 700), color=(10, 150, 77))
+
   def write(self, text, position, color=(0, 0, 0), ttfFile='DroidSansMono.ttf'):
     draw = ImageDraw.Draw(self.img)
     font = ImageFont.truetype(ttfFile, 50)
